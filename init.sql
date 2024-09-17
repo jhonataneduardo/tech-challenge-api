407c682577e9b9eda33cdcec555b3d36cdda46c8
Jhonatan Pereira <jhonatan.pereira@dock.tech>
2024-09-16 23:18:12 -0300

    add colunas script ddl

--- init.sql
+++ init.sql
@@ -65,6 +65,33 @@ CREATE TABLE order_items (
     FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
+-- Criar sequência para tabela payment_types
+CREATE SEQUENCE payment_types_id_seq;
+
+-- Criar tabela payment_types
+CREATE TABLE payment_types (
+	id INTEGER PRIMARY KEY DEFAULT nextval('payment_types_id_seq'),
+	"name" varchar(80) NOT NULL,
+	description text NULL,
+	created_at timestamp NOT NULL
+);
+
+-- Criar sequência para tabela payments
+CREATE SEQUENCE payments_id_seq;
+
+-- Criar tabela payments
+CREATE TABLE payments (
+	id INTEGER PRIMARY KEY DEFAULT nextval('payments_id_seq'),
+	order_id int4 NOT NULL,
+	amount numeric NOT NULL,
+	created_at timestamp NOT NULL,
+	type_id int4 NOT NULL,
+	status int2 NOT NULL,
+	CONSTRAINT payments_unique UNIQUE (order_id),
+    CONSTRAINT payments_orders_fk FOREIGN KEY (order_id) REFERENCES orders(id),
+    CONSTRAINT payments_payment_types_fk FOREIGN KEY (type_id) REFERENCES payment_types(id)
+);
+
 INSERT INTO categories (id, name, created_at, updated_at)
 VALUES (nextval('categories_id_seq'), 'Lanche', NOW(), NULL);
 
@@ -125,3 +152,6 @@ VALUES (nextval('order_items_id_seq'), 2, 1, 16.50, 1.0);
 
 INSERT INTO order_items (id, product_id, order_id, price, quantity)
 VALUES (nextval('order_items_id_seq'), 3, 2, 8.00, 1.0);
+
+INSERT INTO payment_types (id, "name", description, created_at)
+VALUES(nextval('payment_types_id_seq'), 'PIX', 'PIX', NOW());
